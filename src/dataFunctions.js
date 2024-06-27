823f944f7a554ec20f16041cd8a03053cf714007
Alexandra Alarcon <alealarcon067@gmail.com>
2024-06-27 09:38:01 -0500

    pruebas unitarias

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -6,18 +6,18 @@ export function filterDataYear(data, filterValue) {
 
     if (
       (filterValue === '2005-2010' && year >= 2005 && year <= 2010) ||
-      (filterValue === '2010-2015' && year > 2010 && year <= 2015) ||
-      (filterValue === '2015-2020' && year > 2015 && year <= 2020) ||
-      (filterValue === '2020-2023' && year > 2020 && year <= 2023)
+      (filterValue === '2010-2015' && year >= 2010 && year <= 2015) ||
+      (filterValue === '2015-2020' && year >= 2015 && year <= 2020) ||
+      (filterValue === '2020-2023' && year >= 2020 && year <= 2023)
     ) {
       accumulator.push(currentItem);
     }
 
     return accumulator;
-  }, []); 
+  }, []);
+  
   return filteredData;
 }
-
 
 export function filterDataRating(data, filterValue) {
   if (filterValue === 'all') return data;
@@ -29,9 +29,7 @@ export function filterDataRating(data, filterValue) {
     if (filterValue === '1-4') return rating >= 1 && rating <= 4;
     if (filterValue === '4-7') return rating > 4 && rating <= 7;
     if (filterValue === '7-10') return rating > 7 && rating <= 10;
-
   });
-
 }
 
 export function sortData(data, sortValue) {
@@ -46,4 +44,3 @@ export function sortData(data, sortValue) {
 
   return clonedData;
 }
-
